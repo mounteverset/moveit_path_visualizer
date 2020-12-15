c80f09462c671331c8c8abefb2b4dbec1b34675a
Lukas Evers <Lukas.E@web.de>
2020-12-15 14:33:10 +0100

    planner.py debugbar & Pfad für Marker ready

--- rqt_mypkg/src/rqt_mypkg/path_planning_interface.py
+++ rqt_mypkg/src/rqt_mypkg/path_planning_interface.py
@@ -2,6 +2,7 @@
 
 import sys
 import copy
+from moveit_commander import move_group
 import rospy
 import moveit_commander
 import moveit_msgs.msg
@@ -11,6 +12,8 @@ from std_msgs.msg import String
 import io
 import shutil
 import os
+from pathlib import Path
+
 
 class MoveGroupDefinedPath(object):
 
@@ -18,9 +21,12 @@ class MoveGroupDefinedPath(object):
         super(MoveGroupDefinedPath, self).__init__()
 
         moveit_commander.roscpp_initialize(sys.argv)
+        
         rospy.init_node('move_group_defined_path', anonymous=True)
         print("rospy Node started..")
+        
         self.box_name = ""
+        
         self.robot = moveit_commander.RobotCommander()
         self.scene = moveit_commander.PlanningSceneInterface()
         group_name = "panda_arm"
@@ -31,13 +37,26 @@ class MoveGroupDefinedPath(object):
         self.planning_frame = self.move_group.get_planning_frame()
         self.eef_link = self.move_group.get_end_effector_link()
         self.group_names = self.robot.get_group_names()
-    
+
+    def plan_path (self):
+        start_pose = geometry_msgs.msg.Pose()
+        pose = self.get_values(False)
+        start_pose.position.x = float(pose[0])
+        start_pose.position.y = float(pose[1])
+        start_pose.position.z = float(pose[2])
+        start_pose.orientation.w = float(pose[3])   
+        self.move_group.set_pose_target(start_pose)
+        motion_plan = self.move_group.plan()
+
+        return motion_plan
+
     def go_to_starting_pose(self):
         
         print("Current joint_goals:")
         print (self.robot.get_current_state())
 
         start_pose = geometry_msgs.msg.Pose()
+        
         
         #pause zum durchlesen der infos im terminal
         pose = self.get_values(False)
@@ -51,13 +70,13 @@ class MoveGroupDefinedPath(object):
         # better would to set them as inverse kinematic
         
         self.move_group.set_pose_target(start_pose)
-        
+        #self.move_group.plan()
         plan = self.move_group.go(wait=True)
 
         #get rid of any residual movement
         self.move_group.stop()
         self.move_group.clear_pose_targets()
-    
+  
     def go_to_goal_pose(self):
         
         pose_goal = geometry_msgs.msg.Pose()
@@ -76,22 +95,46 @@ class MoveGroupDefinedPath(object):
         self.move_group.clear_pose_targets()
         self.delete_pose_files()
 
+    def display_trajectory (self, plan):
+
+        trajectory = moveit_msgs.msg.DisplayTrajectory()
+        trajectory.trajectory_start = self.robot.get_current_state()
+        trajectory.trajectory.append(plan)
+
+        self.display_trajectory_publisher.publish(trajectory)
+
+    def execute_plan(self, plan):
+        self.move_group.execute(plan, wait=True)
 
     def get_values(self, goal):
+
+        homedir = str(Path.home())
+        start_filepath = homedir + "/starting_pose.txt"
+        goal_filepath = homedir + "/goal_pose.txt"
+
         if (goal == True):
-            with open("goal_pose.txt", "r") as f:
+            with open(start_filepath, "r") as f:
                 pose = [line.rstrip() for line in f]
 
         elif (goal != True):
-            with open("starting_pose.txt", "r") as f:
+            with open(goal_filepath, "r") as f:
                 pose = [line.rstrip() for line in f]
         else:
             return
         return pose   
 
     def delete_pose_files(self):
-        if os.path.exists("goal_pose.txt"):
-           os.remove("goal_pose.txt")
-           os.remove("starting_pose.txt")
+
+        homedir = str(Path.home())
+        start_filepath = homedir + "/starting_pose.txt"
+        goal_filepath = homedir + "/goal_pose.txt"
+
+        if os.path.exists(start_filepath):
+           os.remove(goal_filepath)
+           os.remove(start_filepath)
         else:
            print("The file does not exist")
+
+        
+
+           
